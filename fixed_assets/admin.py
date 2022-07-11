b47b83ddabab2513cc7650ca92cdee6c48eabf09
Luiggi <luirami98@gmail.com>
2022-07-11 12:10:09 -0500

    Añadidos modelos activos fijo, listados y creación

--- fixed_assets/admin.py
+++ fixed_assets/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Ubicacion)
+admin.site.register(Area)
+admin.site.register(Linea)
+admin.site.register(StatusActivo)
+admin.site.register(ActivoFijo)
+
--- fixed_assets/urls.py
+++ fixed_assets/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path, include
-from .views import Ping
+from django.urls import path
+from .views import *
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [   
-path('ping/',Ping.as_view()),
-# path("", include(router.urls)),
+    path('ping/', Ping.as_view()),
+    path('list-ubicacion', ListUbicacion.as_view()),
+    path('list-area', ListArea.as_view()),
+    path('list-linea', ListLinea.as_view()),
+    path('list-status-activo', ListStatusActivo.as_view()),
+    path('detail-a-f', ListDetailActivoFijo.as_view()), # a- f: activo fijo
+    path('create-activo-fijo', CreateActivoFijo.as_view()),
 ]
